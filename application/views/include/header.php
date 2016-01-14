8c2012124ec9c1bfefcf390c453b0a793bd9cf79
pepfi <pepfiwireless@163.com>
2016-01-14 15:35:09 +0800

    init project

---
+++ application/views/include/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>百飞资讯</title>
+    <link rel="icon" href="<?php echo base_url(); ?>style/image/favicon.ico"/>
+    <link href="<?php echo base_url(); ?>application/views/global/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>application/views/global/custom/css/header_footer.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+        <br/>
+        <div class="row">
+            <div class="col-md-2"><a href='/home/index'><img src="<?php echo base_url(); ?>application/views/global/custom/img/logo.png" border='0'></a></div>
+            <div class="col-md-1"></div>
+            <div class="col-md-1"></div>
+            <div class="col-md-1">
+            
+        </div>
+        <div class="row">
+            <div class="col-md-12" style="border-bottom:2px solid #333;margin-top:7px"></div>
+        </div>
+        <div class="row">&nbsp;</div>
+      
+
+
