b22e4538e7c7d67d43d684d359efa3f64cbcb0b5
pepfi <pepfiwireless@163.com>
2016-01-14 17:59:04 +0800

    add header and footer

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,20 +1,16 @@
-        <div class="row">&nbsp;</div>
-        <div class="row">
-            <div class="col-md-12" style="border-bottom:1px solid #333;margin-top:7px"></div>
-        </div>
-        <div class="row">&nbsp;</div>
-        <div class="row">
-            <div class="col-md-2"></div>
-            <div class="col-md-2"></div>
-            <div class="col-md-3">北京傲天动联技术股份有限公司版权所有</div>
-            <div class="col-md-1">©2006-2013</div>
-            <div class="col-md-1">保留所有权利</div>
-            <div class="col-md-1">京 ICP备 </div>
-        </div>
-    </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="<?php echo base_url(); ?>application/views/global/jquery/jquery-1.11.3.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?php echo base_url(); ?>application/views/global/bootstrap/js/bootstrap.min.js"></script>
-  </body>
+        <nav class="navbar-fixed-bottom">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">关于我们 <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">微博</a></li>
+                <li><a href="#">微信公众号</a></li>
+                <li><a href="#">移动端</a></li>
+                <li><a href="#">反馈意见</a></li>
+            </ul>
+        </nav>
+        
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="<?php echo base_url(); ?>application/views/global/jquery/jquery-1.11.3.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="<?php echo base_url(); ?>application/views/global/bootstrap/js/bootstrap.min.js"></script>
+    </body>
 </html>
--- application/views/include/header.php
+++ application/views/include/header.php
@@ -1,28 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>百飞资讯</title>
-    <link rel="icon" href="<?php echo base_url(); ?>style/image/favicon.ico"/>
-    <link href="<?php echo base_url(); ?>application/views/global/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>application/views/global/custom/css/header_footer.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="container">
-        <br/>
-        <div class="row">
-            <div class="col-md-2"><a href='/home/index'><img src="<?php echo base_url(); ?>application/views/global/custom/img/logo.png" border='0'></a></div>
-            <div class="col-md-1"></div>
-            <div class="col-md-1"></div>
-            <div class="col-md-1">
-            
-        </div>
-        <div class="row">
-            <div class="col-md-12" style="border-bottom:2px solid #333;margin-top:7px"></div>
-        </div>
-        <div class="row">&nbsp;</div>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>百飞资讯</title>
+        <meta name="Keywords" content="百飞资讯网,关注互联网公司技术、产品、人物"/>
+        <meta name="Description" content="持续关注互联网公司,技术创新公司,优质上市公司的方方面面" />
+
+        <link rel="icon" href="<?php echo base_url(); ?>style/image/favicon.ico"/>
+        <link href="<?php echo base_url(); ?>application/views/global/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?php echo base_url(); ?>application/views/global/custom/css/header_footer.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="/home">百飞资讯</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">首页 <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">发现</a></li>
+                <li><a href="#">分类</a></li>
+             </ul>
+              <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+              </form>
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="">登陆</a></li>
+                <li><a href="">注册</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+   
       
 
 
